a4ac8020f79e30451378527f661ee5eda7007f8e
samuelalex93 <smk.tecinfo@gmail.com>
2026-05-30 18:17:18 -0300

    feat: add new funcs

--- ui-sistema-palestras/src/app/cadastro/cadastro.ts
+++ ui-sistema-palestras/src/app/cadastro/cadastro.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
+interface respostaCadastro {
+  message: string;
+}
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {}
+
+export class Cadastro {
+
+  constructor(private http: HttpClient, private router: Router, private cdr: ChangeDetectorRef) { }
+
+  formularioCadastro = new FormGroup({
+    email: new FormControl('', Validators.required),
+    nome: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    senha: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8)
+    ])
+  });
+
+  mensagem: string = '';
+  tipoMensagem: 'success' | 'danger' = 'success';
+
+  onSubmit() {
+    if (this.formularioCadastro.valid) {
+      this.http
+        .post<respostaCadastro>(
+          'http://localhost:3000/api/cadastro',
+          this.formularioCadastro.value
+        )
+        .subscribe({
+          next: (res) => {
+            this.tipoMensagem = 'success';
+            this.mensagem = res.message;
+            this.cdr.detectChanges();
+            setTimeout(() => {
+              this.router.navigate(['/login']);
+            }, 2000);
+          },
+          error: (err) => {
+            this.tipoMensagem = 'danger';
+            if (err?.status === 400) {
+              this.mensagem = err?.error?.message || 'Email já cadastrado';
+            } else {
+              this.mensagem = err?.error?.message || 'Erro ao cadastrar';
+            }
+            this.cdr.detectChanges();
+          },
+        });
+    }
+  }
+}
