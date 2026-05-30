d0daef95e9fd446496ae8e648ff8ff021365dff6
samuelalex93 <smk.tecinfo@gmail.com>
2026-05-30 18:18:08 -0300

    feat: improve inteface and infos home

--- ui-sistema-palestras/src/app/home/home.ts
+++ ui-sistema-palestras/src/app/home/home.ts
@@ -1,9 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../auth';
+import { Router } from '@angular/router';
 
+import { PalestraApi, Palestra } from "../palestra";
+import { ChangeDetectorRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+interface UserData {
+  id: number;
+  email: string;
+  nome: string;
+  admin: boolean;
+}
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  palestras: Palestra[] = [];
+  userData: UserData | null = null;
+
+  constructor(
+    private authService: Auth,
+    private router: Router,
+    private palestraApi: PalestraApi,
+    private cd: ChangeDetectorRef,
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.userData = this.authService.getUser();
+    if (!this.userData) {
+      this.router.navigateByUrl("/login");
+    }
+    this.palestraApi.listarPalestra().subscribe({
+      next: (dados) => {
+        this.palestras = dados;
+        this.cd.detectChanges();
+      },
+      error: (err) => console.log("Erro ao carregar dados", err),
+    });
+
+  }
+
+  inscricao(idPalestra: number): void {
+    const idUsuario = this.userData?.id;
+    if (!idUsuario) return;
+    this.http.post<any>("http://localhost:3000/api/inscricao", {
+      idUsuario,
+      idPalestra
+    }).subscribe({
+      next: (res) => {
+        alert(res.message);
+      },
+      error: (err) => {
+        alert(err.error.message);
+      }
+    });
+  }
+}
