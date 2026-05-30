82f1606cf4552978114ab8595372f3e13f200472
samuelalex93 <smk.tecinfo@gmail.com>
2026-05-30 18:19:00 -0300

    feat: add new components

--- ui-sistema-palestras/src/app/app.routes.ts
+++ ui-sistema-palestras/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { Cadastro } from './cadastro/cadastro';
 import { Login } from './login/login';
 import { Home } from './home/home';
-//import { CadastrarEvento } from './cadastrar-evento/cadastrar-evento';
+import { CadastrarEvento } from './cadastrar-evento/cadastrar-evento';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'cadastro', component: Cadastro },
   { path: 'login', component: Login },
   { path: 'home', component: Home },
-  //{ path: 'admin', component: CadastrarEvento }
+  { path: 'admin', component: CadastrarEvento }
 ];
