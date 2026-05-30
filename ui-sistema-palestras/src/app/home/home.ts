f28646f8ce49f08244e40527388e3117c6b08932
samuelalex93 <smk.tecinfo@gmail.com>
2026-05-30 19:23:07 -0300

    feat: improve style and add find my subscribes

--- ui-sistema-palestras/src/app/home/home.ts
+++ ui-sistema-palestras/src/app/home/home.ts
@@ -6,12 +6,20 @@ import { Router } from '@angular/router';
 import { PalestraApi, Palestra } from "../palestra";
 import { ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
 interface UserData {
   id: number;
   email: string;
   nome: string;
   admin: boolean;
 }
+
+interface Inscricao {
+  ID: number;
+  idUsuario: number;
+  idPalestra: number;
+}
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -22,6 +30,7 @@ interface UserData {
 export class Home implements OnInit {
   palestras: Palestra[] = [];
   userData: UserData | null = null;
+  minhasInscricoes: Inscricao[] = [];
 
   constructor(
     private authService: Auth,
@@ -36,6 +45,7 @@ export class Home implements OnInit {
     if (!this.userData) {
       this.router.navigateByUrl("/login");
     }
+
     this.palestraApi.listarPalestra().subscribe({
       next: (dados) => {
         this.palestras = dados;
@@ -44,17 +54,40 @@ export class Home implements OnInit {
       error: (err) => console.log("Erro ao carregar dados", err),
     });
 
+    if (this.userData && !this.userData.admin) {
+      this.carregarMinhasInscricoes();
+    }
+  }
+
+  carregarMinhasInscricoes(): void {
+    if (!this.userData) return;
+
+    this.http.get<Inscricao[]>(
+      `http://localhost:3000/api/inscricoes/usuario/${this.userData.id}`
+    ).subscribe({
+      next: (inscricoes) => {
+        this.minhasInscricoes = inscricoes;
+        this.cd.detectChanges();
+      },
+      error: (err) => console.log("Erro ao carregar inscrições", err),
+    });
+  }
+
+  estaInscrito(idPalestra: number): boolean {
+    return this.minhasInscricoes.some(i => i.idPalestra === idPalestra);
   }
 
   inscricao(idPalestra: number): void {
     const idUsuario = this.userData?.id;
     if (!idUsuario) return;
+
     this.http.post<any>("http://localhost:3000/api/inscricao", {
       idUsuario,
       idPalestra
     }).subscribe({
       next: (res) => {
         alert(res.message);
+        this.carregarMinhasInscricoes();
       },
       error: (err) => {
         alert(err.error.message);
