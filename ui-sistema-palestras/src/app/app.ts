096cde87a43955453a50f58bcfd67b2070effbdc
samuelalex93 <smk.tecinfo@gmail.com>
2026-05-30 18:19:41 -0300

    feat:  create login form

--- ui-sistema-palestras/src/app/app.ts
+++ ui-sistema-palestras/src/app/app.ts
@@ -1,13 +1,40 @@
-import { Component, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, signal, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive, Router, NavigationEnd }
+  from '@angular/router';
+import { Auth } from './auth';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
+interface UserData {
+  email: string;
+  nome: string;
+  admin: boolean;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('ui-sistema-palestras');
+  userData: UserData | null = null;
+  logado: boolean = false;
+  constructor(private router: Router, private authService: Auth) { }
+  ngOnInit(): void {
+    this.logado = this.authService.isLoggedIn();
+    this.userData = this.authService.getUser();
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.logado = this.authService.isLoggedIn();
+        this.userData = this.authService.getUser();
+      });
+  }
+  logout(): void {
+    this.authService.logout();
+    this.logado = false;
+    this.userData = null;
+  }
 }
--- ui-sistema-palestras/src/app/login/login.ts
+++ ui-sistema-palestras/src/app/login/login.ts
@@ -1,9 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from
+  '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Auth } from '../auth';
 
+interface respostaLogin {
+  message: string;
+  tipoMensagem: string;
+  userData: {
+    id: number;
+    email: string;
+    nome: string;
+    admin: boolean;
+  };
+}
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private cdr: ChangeDetectorRef,
+    private authService: Auth
+  ) { }
+  formularioLogin = new FormGroup({
+    email: new FormControl('', Validators.required),
+    senha: new FormControl('', [Validators.required,
+    Validators.minLength(8)]),
+  });
+
+  mensagem: string = '';
+  tipoMensagem: string = '';
+  userData: { email: string; nome: string; admin: boolean } = {
+    email: '',
+    nome: '',
+    admin: false,
+  };
+
+  onSubmit() {
+
+    if (this.formularioLogin.valid) {
+      this.http
+        .post<respostaLogin>(
+          'http://localhost:3000/api/login',
+          this.formularioLogin.value
+        )
+        .subscribe({
+          next: (res) => {
+            this.tipoMensagem = (res.tipoMensagem as any) || 'success';
+            this.mensagem = res.message || 'Login realizado com sucesso!';
+            this.cdr.detectChanges();
+            if (res.tipoMensagem === 'success') {
+              this.authService.login(res.userData);
+              this.router.navigateByUrl('/home');
+            }
+          },
+          error: (err) => {
+            this.tipoMensagem = err.error?.tipoMensagem || 'danger';
+            this.mensagem = err.error?.message || 'Usuário ou senha inválidos';
+            this.cdr.detectChanges();
+          },
+        });
+    }
+  }
+}
