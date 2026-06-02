aa25ae2b052a61043c5076f0213fe6e4ef15c9f5
samuelalex93 <smk.tecinfo@gmail.com>
2026-06-02 12:56:24 -0300

    infra: add script sql to run when setup mysql

--- infra/scripts.sql
+++ infra/scripts.sql
@@ -6,10 +6,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
  admin BOOLEAN DEFAULT 0
 );
 
-INSERT INTO usuarios(email,nome,admin, senha) VALUES ('teste@gmail.com', 'teste',
-1, '12345678');
-
-Create table palestra(
+CREATE TABLE IF NOT EXISTS palestra(
 id int primary key auto_increment,
 titulo varchar(255),
 descricao varchar(255),
@@ -18,7 +15,7 @@ localEvento varchar (255),
 dataEvento TIMESTAMP
 )
 
-CREATE TABLE inscricoes (
+CREATE TABLE IF NOT EXISTS inscricoes (
  id INT PRIMARY KEY AUTO_INCREMENT,
  idUsuario INT,
  idPalestra INT,
